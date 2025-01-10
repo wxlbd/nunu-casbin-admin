102183cbfbe87dc982f7d763fbb53797f709c8d9
wxl <297750182@qq.com>
2025-01-10 15:20:29 +0800

    feat(menu): 添加菜单树结构转换函数并更新菜单响应结构
    - 在 menu.go 中添加 ToMenuTree 和 ToMenuTreeItem 函数，用于将服务层菜单树转换为响应层菜单树
    - 更新 menu.go 中的 MenuTree 结构，增加创建时间和更新时间字段
    - 重构 response/menu.go 中的菜单响应结构，简化为 MenuTree 和 MenuTreeResponse

--- internal/dto/menu.go
+++ internal/dto/menu.go
@@ -3,6 +3,7 @@ package dto
 import (
 	"github.com/wxlbd/nunu-casbin-admin/internal/handler/response"
 	"github.com/wxlbd/nunu-casbin-admin/internal/model"
+	"github.com/wxlbd/nunu-casbin-admin/internal/service"
 	"time"
 )
 
@@ -28,5 +29,31 @@ func ToMenu(menu *model.Menu) *response.Menu {
 		Status:     menu.Status,
 		CreateTime: menu.CreatedAt.Format(time.DateTime),
 		UpdateTime: menu.UpdatedAt.Format(time.DateTime),
+	}
+}
+
+func ToMenuTree(menus []*service.MenuTree) []*response.MenuTree {
+	var menuList []*response.MenuTree
+	for _, menu := range menus {
+		menuList = append(menuList, ToMenuTreeItem(menu))
+	}
+	return menuList
+}
+
+func ToMenuTreeItem(menu *service.MenuTree) *response.MenuTree {
+	if menu == nil {
+		return nil
+	}
+	return &response.MenuTree{
+		ID:         menu.ID,
+		ParentID:   menu.ParentID,
+		Name:       menu.Name,
+		Path:       menu.Path,
+		Component:  menu.Component,
+		Sort:       menu.Sort,
+		Status:     menu.Status,
+		CreateTime: menu.CreatedAt.Format(time.DateTime),
+		UpdateTime: menu.UpdatedAt.Format(time.DateTime),
+		Children:   ToMenuTree(menu.Children),
 	}
 }
--- internal/handler/response/menu.go
+++ internal/handler/response/menu.go
@@ -1,24 +1,23 @@
 package response
 
-// 菜单树节点
+// MenuTree 菜单树节点
 type MenuTree struct {
-	ID        uint64      `json:"id"`
-	ParentID  uint64      `json:"parent_id"`
-	Name      string      `json:"name"`
-	Path      string      `json:"path"`
-	Component string      `json:"component"`
-	Sort      int16       `json:"sort"`
-	Status    int8        `json:"status"`
-	Children  []*MenuTree `json:"children,omitempty"`
+	ID         uint64      `json:"id"`
+	ParentID   uint64      `json:"parent_id"`
+	Name       string      `json:"name"`
+	Path       string      `json:"path"`
+	Component  string      `json:"component"`
+	Sort       int16       `json:"sort"`
+	Status     int8        `json:"status"`
+	CreateTime string      `json:"create_time"`
+	UpdateTime string      `json:"update_time"`
+	Children   []*MenuTree `json:"children,omitempty"`
 }
 
-// 菜单列表响应
-type MenuListResponse struct {
-	List  []*MenuTree `json:"list"`
-	Total int64       `json:"total"`
-}
+// MenuTreeResponse 菜单列表响应
+type MenuTreeResponse []*MenuTree
 
-// 菜单信息响应
+// Menu 菜单信息响应
 type Menu struct {
 	ID         uint64 `json:"id"`
 	ParentID   uint64 `json:"parent_id"`
