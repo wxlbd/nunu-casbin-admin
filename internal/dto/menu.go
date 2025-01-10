967a26964e31f95ed2b0a5bfb63a986543806f38
wxl <297750182@qq.com>
2025-01-10 17:31:25 +0800

    refactor(menu): 重构菜单相关 DTO
    - 重构了 ToMenuTree 函数，改为返回 []*response.MenuTreeNode
    - 新增 ToMenuTreeNode 函数，用于转换单个菜单节点
    - 新增 ToMenuMeta 函数，用于转换菜单元数据
    - 更新了相关的数据结构和命名

--- internal/dto/menu.go
+++ internal/dto/menu.go
@@ -1,10 +1,11 @@
 package dto
 
 import (
+	"time"
+
 	"github.com/wxlbd/nunu-casbin-admin/internal/handler/response"
 	"github.com/wxlbd/nunu-casbin-admin/internal/model"
 	"github.com/wxlbd/nunu-casbin-admin/internal/service"
-	"time"
 )
 
 func ToMenuList(menus []*model.Menu) []*response.Menu {
@@ -32,28 +33,44 @@ func ToMenu(menu *model.Menu) *response.Menu {
 	}
 }
 
-func ToMenuTree(menus []*service.MenuTree) []*response.MenuTree {
-	var menuList []*response.MenuTree
+func ToMenuTree(menus []*service.MenuTree) []*response.MenuTreeNode {
+	var menuList []*response.MenuTreeNode
 	for _, menu := range menus {
-		menuList = append(menuList, ToMenuTreeItem(menu))
+		menuList = append(menuList, ToMenuTreeNode(menu))
 	}
 	return menuList
 }
 
-func ToMenuTreeItem(menu *service.MenuTree) *response.MenuTree {
+func ToMenuTreeNode(menu *service.MenuTree) *response.MenuTreeNode {
 	if menu == nil {
 		return nil
 	}
-	return &response.MenuTree{
-		ID:         menu.ID,
-		ParentID:   menu.ParentID,
-		Name:       menu.Name,
-		Path:       menu.Path,
-		Component:  menu.Component,
-		Sort:       menu.Sort,
-		Status:     menu.Status,
-		CreateTime: menu.CreatedAt.Format(time.DateTime),
-		UpdateTime: menu.UpdatedAt.Format(time.DateTime),
-		Children:   ToMenuTree(menu.Children),
+	return &response.MenuTreeNode{
+		ID:        menu.ID,
+		ParentID:  menu.ParentID,
+		Name:      menu.Name,
+		Path:      menu.Path,
+		Component: menu.Component,
+		Meta:      ToMenuMeta(menu.Meta),
+		Sort:      menu.Sort,
+		Status:    menu.Status,
+		CreatedAt: menu.CreatedAt.Format(time.DateTime),
+		UpdatedAt: menu.UpdatedAt.Format(time.DateTime),
+		Children:  ToMenuTree(menu.Children),
+	}
+}
+func ToMenuMeta(meta model.MenuMeta) response.Meta {
+	return response.Meta{
+		I18N:             meta.I18n,
+		Icon:             meta.Icon,
+		Type:             meta.Type,
+		Affix:            meta.Affix,
+		Cache:            meta.Cache,
+		Title:            meta.Title,
+		Hidden:           meta.Hidden,
+		Copyright:        meta.Copyright,
+		ComponentPath:    meta.ComponentPath,
+		ComponentSuffix:  meta.ComponentSuffix,
+		BreadcrumbEnable: meta.BreadcrumbEnable,
 	}
 }
