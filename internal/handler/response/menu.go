b14299d39c45c77742cf5fc01b800e0122fdefe4
wxl <297750182@qq.com>
2025-01-10 17:31:53 +0800

    refactor(menu): 重构菜单响应结构体
    - 将 MenuTree 重命名为 MenuTreeNode，以更准确地表示菜单树节点
    - 新增 Meta 结构体，用于存储菜单的元数据信息
    - 在 MenuTreeNode 中添加 Meta 字段和其他新字段，以支持更多的菜单属性
    - 更新 Menu 结构体，将 Meta 作为独立字段返回

--- internal/handler/response/menu.go
+++ internal/handler/response/menu.go
@@ -1,21 +1,40 @@
 package response
 
 // MenuTree 菜单树节点
-type MenuTree struct {
-	ID         uint64      `json:"id"`
-	ParentID   uint64      `json:"parent_id"`
-	Name       string      `json:"name"`
-	Path       string      `json:"path"`
-	Component  string      `json:"component"`
-	Sort       int16       `json:"sort"`
-	Status     int8        `json:"status"`
-	CreateTime string      `json:"create_time"`
-	UpdateTime string      `json:"update_time"`
-	Children   []*MenuTree `json:"children,omitempty"`
+type MenuTreeNode struct {
+	ID        uint64          `json:"id"`
+	ParentID  uint64          `json:"parent_id"`
+	Name      string          `json:"name"`
+	Meta      Meta            `json:"meta"`
+	Path      string          `json:"path"`
+	Component string          `json:"component"`
+	Redirect  string          `json:"redirect"`
+	Status    int8            `json:"status"`
+	Sort      int16           `json:"sort"`
+	CreatedBy int             `json:"created_by"`
+	UpdatedBy int             `json:"updated_by"`
+	CreatedAt string          `json:"created_at"`
+	UpdatedAt string          `json:"updated_at"`
+	Remark    string          `json:"remark"`
+	Children  []*MenuTreeNode `json:"children"`
+}
+
+type Meta struct {
+	I18N             string `json:"i18n"`
+	Icon             string `json:"icon"`
+	Type             string `json:"type"`
+	Affix            bool   `json:"affix"`
+	Cache            bool   `json:"cache"`
+	Title            string `json:"title"`
+	Hidden           bool   `json:"hidden"`
+	Copyright        bool   `json:"copyright"`
+	ComponentPath    string `json:"componentPath"`
+	ComponentSuffix  string `json:"componentSuffix"`
+	BreadcrumbEnable bool   `json:"breadcrumbEnable"`
 }
 
 // MenuTreeResponse 菜单列表响应
-type MenuTreeResponse []*MenuTree
+type MenuTreeResponse []*MenuTreeNode
 
 // Menu 菜单信息响应
 type Menu struct {
@@ -26,6 +45,7 @@ type Menu struct {
 	Component  string `json:"component"`
 	Sort       int16  `json:"sort"`
 	Status     int8   `json:"status"`
+	Mate       any    `json:"mate"`
 	CreateTime string `json:"create_time"`
 	UpdateTime string `json:"update_time"`
 }
